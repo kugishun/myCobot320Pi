10d7f6ccda539adf7a6cf98ac9f8eb6e6ffea03b
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-09-19 14:20:42 +0900

    check connecting

--- LEDchange.py
+++ LEDchange.py
@@ -3,5 +3,7 @@ from pymycobot import PI_PORT, PI_BAUD
 
 mc = MyCobot("/dev/ttyAMA0", 115200)
 print(str(PI_PORT) + "," + str(PI_BAUD))
-print(mc.is_connected())
+# print(mc.is_connected())
+angles = mc.get_angles()
+print(angles)
 mc.set_color(255, 0, 0)
