72045f6c56142d4672d4ca55dbca34f03591a1f8
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-09-24 12:40:29 +0900

    reboot.pyを作成(テスト)

---
+++ reboot.py
@@ -0,0 +1,14 @@
+from pymycobot.mycobot import MyCobot
+from pymycobot import PI_PORT, PI_BAUD
+from pymycobot.genre import Angle
+import time
+
+mc = MyCobot(PI_PORT, 115200)
+
+mc.power_off()
+mc.power_on()
+
+mc.send_angles([-124, 130, -4, -2, 7, -1.5], 50)
+time.sleep(12.5)
+
+mc.release_all_servos()
