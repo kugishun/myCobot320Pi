5da3aa806f07edf6a6f13ab8376b7cf963e0824f
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-09-24 11:22:28 +0900

    MyCobot()の中身を変更

--- LEDchange.py
+++ LEDchange.py
@@ -1,7 +1,7 @@
 from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
 
-mc = MyCobot("/dev/ttyAMA0", 115200)
+mc = MyCobot(PI_PORT, 115200)
 print(str(PI_PORT) + "," + str(PI_BAUD))
 # print(mc.is_connected())
 angles = mc.get_angles()
