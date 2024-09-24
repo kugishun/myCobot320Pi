b3ffc346f2eaa77d609d1471b27689d99d881e99
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-09-24 11:28:21 +0900

    PI_BAUDについてのメモを追加

--- LEDchange.py
+++ LEDchange.py
@@ -1,7 +1,8 @@
 from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
 
-mc = MyCobot(PI_PORT, PI_BAUD)
+mc = MyCobot(PI_PORT, 115200)
+# MyCobot(PI_PORT,PI_BAUD)とするとPI_BAUDの中身である1000000が大きすぎるため反応しない
 print(str(PI_PORT) + "," + str(PI_BAUD))
 # print(mc.is_connected())
 angles = mc.get_angles()
