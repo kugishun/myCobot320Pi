f85c448ba3d80d1f9aa3a8faee29fd2f09718ac8
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-11-13 17:34:17 +0900

    handle_controller.pyの作成

---
+++ handle_controller.py
@@ -0,0 +1,185 @@
+import pygame
+import pygame.locals
+import time
+import threading
+from pymycobot.mycobot import MyCobot
+from pymycobot.genre import Angle
+
+zero = [0, 0, 0, 0, 0, 0]  # -170 to 170
+arm_state = zero
+action = 0
+command = -1
+status = 0
+
+mc = MyCobot("/dev/ttyAMA0", 115200)
+
+
+def control():
+    global action, status, command
+    select_shaft = 1
+    status = 1
+    angle = 0
+    grip = False
+    while True:
+        if status == 1:
+            if action == 1:  # アームの現在の座標を出力
+                position = mc.get_angles()
+                print("現在の状態 :{}".format(position))
+                action = 0
+            elif action == 2:  # 軸を動かすモードに移動
+                print("軸が決定されました: {}".format(str(select_shaft)))
+                status = 2
+            elif action == 4:  # 動かす軸の選択(増加)
+                if select_shaft < 6:
+                    select_shaft += 1
+                elif select_shaft >= 6:
+                    select_shaft = 1
+                print("選択されている軸: {}".format(str(select_shaft)))
+                action = 0
+            elif action == 5:  # 動かす軸の選択(減少)
+                if select_shaft > 1:
+                    select_shaft -= 1
+                elif select_shaft <= 1:
+                    select_shaft = 6
+                print("選択されている軸: {}".format(str(select_shaft)))
+                action = 0
+            elif action == 6:
+                print("action :{}".format(str(action)))
+                action = 0
+
+        elif status == 2:
+            if action == 1:  # アームの現在の座標を出力
+                position = mc.get_angles()
+                print("現在の状態 :{}".format(position))
+                action = 0
+            elif command == 0:  # 選択されている軸の移動(マイナス方向)
+                print("軸をマイナス方向に移動します。")
+                if arm_state[select_shaft] > -165:
+                    arm_state[select_shaft] -= 1
+                    mc.send_angle(select_shaft, arm_state[select_shaft], 50)
+                    time.sleep(1)
+                else:
+                    arm_state[select_shaft] = -165
+                # command = -1
+            elif command == 1:  # 選択されている軸の移動(プラス)
+                print("軸をプラス方向に移動します。")
+                if arm_state[select_shaft] < 165:
+                    arm_state[select_shaft] += 1
+                    mc.send_angle(select_shaft, arm_state[select_shaft], 50)
+                    time.sleep(1)
+                else:
+                    arm_state[select_shaft] = 165
+                # command = -1
+            elif action == 3:  # 軸の選択モードへ戻る
+                print("軸の選択画面に戻ります。")
+                status = 1
+                action = 0
+            elif action == 6:
+                print("action :{}".format(str(action)))
+                action = 0
+            elif action == 7:  # グリップの開閉
+                print("グリップの開閉を行います")
+                grip = not grip
+                mc.set_eletric_gripper(grip)
+                action = 0
+
+
+def main():
+    global action, status, command
+    flag = 0
+    pygame.init()
+    pygame.joystick.init()
+
+    try:
+        joystick = pygame.joystick.Joystick(0)
+    except:
+        print("Please connect the handle first.")
+        return
+    joystick.init()
+
+    done = False
+
+    start_time = 0
+    while not done:
+        for event_ in pygame.event.get():
+            if event_.type == pygame.QUIT:
+                done = True
+            elif (
+                event_.type == pygame.JOYBUTTONDOWN or event_.type == pygame.JOYBUTTONUP
+            ):
+                buttons = joystick.get_numbuttons()
+                for i in range(buttons):
+                    button = joystick.get_button(i)
+                    if status == 1:
+                        if i == 3:  # Y
+                            if button == 1:
+                                action = 1
+                                # print("action 1")
+                                break
+                        elif i == 0:  # A
+                            if button == 1:
+                                action = 2
+                                # print("action 2")
+                                break
+                        if i == 2:  # X
+                            if button == 1:
+                                # print("action 6")
+                                action = 6
+                                break
+                    elif status == 2:
+                        if i == 1:  # B
+                            if button == 1:
+                                action = 3
+                                # print("action 3")
+                                break
+                        elif i == 2:  # X
+                            if button == 1:
+                                # print("action 6")
+                                action = 6
+                                break
+                        elif i == 0:  # A
+                            if button == 1:
+                                action = 7
+                                break
+
+            elif event_.type == pygame.JOYHATMOTION:
+                hat = joystick.get_hat(0)
+                if status == 1:
+                    if hat == (1, 0):
+                        action = 4
+                        # print("action 4")
+                        break
+                    elif hat == (-1, 0):
+                        action = 5
+                        # print("action 5")
+                        break
+
+            elif event_.type == pygame.JOYAXISMOTION:
+                axes = joystick.get_numaxes()
+                for i in range(axes):
+                    axis = joystick.get_axis(i)
+                    if status == 2:
+                        if i == 4:
+                            if axis > 0.5:
+                                command = 0
+                                # print("command 0")
+                                flag = 1
+                                break
+                            else:
+                                command = -1
+                        elif i == 5:
+                            if axis > 0.5:
+                                command = 1
+                                # print("command 1")
+                                flag = 1
+                                break
+                            else:
+                                command = -1
+
+
+if __name__ == "__main__":
+    t = threading.Thread(target=control)
+    t.daemon = True
+    t.start()
+    print("test")
+    main()
