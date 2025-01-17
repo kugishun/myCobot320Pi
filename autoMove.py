9060605d32bd7ce052ecf950266a96d831674b2f
kugimiya shun <s1290195@u-aizu.ac.jp>
2025-01-17 15:58:26 +0900

    autoMoveファイルを作成

---
+++ autoMove.py
@@ -0,0 +1,28 @@
+from pymycobot.mycobot import MyCobot
+from pymycobot.genre import Angle
+import time
+
+mc = MyCobot("/dev/ttyAMA0", 115200)
+mc.init_eletric_gripper()
+
+mc.power_off()
+mc.power_on()
+
+mc.send_angles([0, 0, 0, 0, 0, 0], 50)
+time.sleep(5)
+
+mc.send_angle([-18, 84.5, 22, 24.5, 0, 96], 50)
+time.sleep(5)
+mc.set_eletric_gripper(1)
+mc.set_eletric_gripper(1)
+mc.set_eletric_gripper(1)
+
+mc.send_angle([-18, 85, 3, 24.5, 0, 96], 50)
+time.sleep(10)
+mc.send_angle([-18, 84.5, 22, 24.5, 0, 96], 50)
+time.sleep(10)
+mc.set_electric_gripper(0)
+mc.set_electric_gripper(0)
+mc.set_electric_gripper(0)
+mc.send_angles([0, 0, 0, 0, 0, 0], 50)
+time.sleep(5)
