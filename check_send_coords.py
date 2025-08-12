2bbdcd9c15f70373b49b564100c374ff5b6c70b9
kugishun <kugishungithub@gmail.com>
2025-08-12 18:55:27 +0900

    令和最新版

--- check_send_coords.py
+++ check_send_coords.py
@@ -1,24 +1,25 @@
-from pymycobot import MyCobot
-from pymycobot.genre import Coord
+from pymycobot.mycobot320 import MyCobot320
 import time
 # import the project package
-
 # Initiate a MyPalletizer object
-mc = MyCobot("/dev/ttyAMA0", 115200)
+mc = MyCobot320("/dev/ttyAMA0", 115200)
 
 # # Get the current coordinates and pose of the head
 # coords = mc.get_coords()
 # print(coords)
 
 #Plan the route at random, let the head reach the coordinates of [57.0, -107.4, 316.3] in an non-linear manner at the speed is 80mm/s
-mc.send_coords([200,0,350,0,0,0],50,1)
+mc.send_coords([22.82, -246.83, 421.77, -63.78, 85.64, 17.65],50,1)
 # wait for 2 seconds
 time.sleep(2)
 
 # Plan the route at random, let the head reach the coordinates of [207.9, 47, 49.3,-159.69] in an non-linear manner at the speed is 80mm/s
-# mc.send_coords([207.9, 47, 49.3,-159.69], 80, 0)
+mc.send_coords([-322.23, -206.54, 180.52, 64.37, 162.74, -173.04], 80, 0)
 # wait for 2 seconds
 time.sleep(2)
 
 #To change only the x-coordinate of the head, set the x-coordinate of the head to 20. Let it plan the route at random and move the head to the changed position at a speed of 70mm/s
-mc.send_coord(Coord.X.value, 20, 50)
+mc.send_coords([98.14, 232.62, 166.24, -138.59, -147.04, -50.08], 80, 0)
+
+time.sleep(2)
+print("finish")
