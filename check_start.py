be91dc83f5bdde4ed15926e3f26b1a36ef12681c
kugishun <kugishungithub@gmail.com>
2025-08-16 17:30:41 +0900

    座標の取得

--- check_start.py
+++ check_start.py
@@ -5,6 +5,16 @@ import time
 mc = MyCobot320(PI_PORT, 115200)
 
 mc.send_angles([0, 0, 0, 0, 0, 0], 50)
+time.sleep(1)
+coords = mc.get_coords()
+print(f"get_coords():{coords}")
 
+mc.send_angles([90, 0, 0, 0, 0, 0], 50)
+time.sleep(1)
+coords = mc.get_coords()
+print(f"get_coords():{coords}")
+
+mc.send_angles([-90, 0, 0, 0, 0, 0], 50)
+time.sleep(2)
 coords = mc.get_coords()
 print(f"get_coords():{coords}")
