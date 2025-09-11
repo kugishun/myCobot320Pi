8e011024efac8d223d8f2341a7beb6b9be947568
kugishun <kugishungithub@gmail.com>
2025-09-11 20:55:27 +0900

    変更

--- arm_H100/test_h100.py
+++ arm_H100/test_h100.py
@@ -1,5 +1,17 @@
 from MyHand import MyGripper_H100
+import time
+if __name__=="__main__":
+    hand=MyGripper_H100("/dev/ttyCH343USB0")
+    hand.set_gripper_pose(0,0)
+    time.sleep(2)
+    hand.set_gripper_pose(1,5)
+    time.sleep(5)
+    hand.set_gripper_pose(2,5)
+    time.sleep(5)
+    hand.set_gripper_pose(3,5)
+    time.sleep(5)
+    hand.set_gripper_pose(4,15)
+    time.sleep(5)
+    hand.set_gripper_pose(0,0)
+    time.sleep(2)
 
-
-hand = MyGripper_H100("COM6")
-hand.set_gripper_pose(0, 0)
